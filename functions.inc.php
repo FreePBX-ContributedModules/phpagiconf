cc6f7860b759c0e5106c00873cf2772aa6af1e4c
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 15:26:26 -0500

    update License

--- functions.inc.php
+++ functions.inc.php
@@ -1,6 +1,8 @@
 <?php /* $Id: $ */
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
 function phpagiconf_gen_conf() {
 	global $active_modules;
 
